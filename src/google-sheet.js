a05bdfc53f4e4b482ac8a483cda3bf7882155b16
muhdanees <anees.office@gmail.com>
2019-03-12 22:29:39 +0530

    validations

--- src/google-sheet.js
+++ src/google-sheet.js
@@ -34,10 +34,10 @@ $("#foo").submit(function(event) {
     // Callback handler that will be called on success
     request.done(function(response, textStatus, jqXHR) {
         // Log a message to the console
-        console.log("Hooray, it worked!");
-        console.log(response);
-        console.log(textStatus);
-        console.log(jqXHR);
+        // console.log("Hooray, it worked!");
+        // console.log(response);
+        // console.log(textStatus);
+        // console.log(jqXHR);
 
         $form[0].reset();
     });
