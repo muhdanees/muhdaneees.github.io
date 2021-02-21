1231e7c939d51a14f82152275bbe110dd10be217
m58045 <mohd.anees@htdigital.in>
2021-02-22 00:15:20 +0530

    home page changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 
 import Home from "./pages/home";
 import About from "./pages/about";
@@ -12,9 +12,9 @@ import Contact from "./pages/contact";
 import NavigationSide from "./Components/Navigation";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faToolbox } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-import "./index.css";
+import "./index.scss";
 
 const App = () => {
     return (
@@ -24,16 +24,16 @@ const App = () => {
                 <div className="Codeplayer">
                     <div className="Header">
                         <div className="InfoSec">
-                            <div className="Logo"><a href="./">Codeplayer</a></div>
-                            <NavigationSide />
-                        </div>
-                        <div className="AboutInfo">
-                            <div className="Container">
-                                <h1>Hi, I’m Mohd Anees</h1>
-                                <div className="Work-postion"><FontAwesomeIcon icon={faToolbox} />Currently workin as <strong>Front-end Developer</strong> in HT Media Limited.</div>
-                                <p>11+ Years of Industry experience in providing User Interface Designing/Development  with most usable enterprise wide applications. Described as the Education of websites,  UI Developers are responsible for writing the code that lives behind every webpage.  Using their creativity and technical knowledge, they plan, build and maintain the structure of a website.</p>
-                                <a href="./contact-us/" className="contact-us">Contact me!</a>
+                            <div className="Logo">
+                                <Link to="./">
+                                    <img src='/mini-logo.svg' alt="" />
+                                    <span>Codeplayer</span>
+                                </Link>
                             </div>
+                            <div className="home-icon">
+                                <FontAwesomeIcon icon={faBars} />
+                            </div>
+                            <NavigationSide />
                         </div>
                     </div>
                     <Switch>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,18 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
 function Home() {
   return (
     <>
-      <div className="Container">
-        <div className="SocialNet">
-          <div>linkedin</div>
-          <div>github</div>
-          <div>twitter</div>
-          <div>facebook</div>
+        <div className="AboutInfo">
+            <div className="Container">
+                <img className="about-me" src='/about-bg.jpg' alt="" />
+                <h1>Mohd Anees</h1>
+                {/* <FontAwesomeIcon icon={faToolbox} /> */}
+                <div className="Work-postion">Front-end Developer</div>
+                <p>11+ Years of Industry experience in providing User Interface Designing/Development  with most usable enterprise wide applications. Described as the Education of websites,  UI Developers are responsible for writing the code that lives behind every webpage.  Using their creativity and technical knowledge, they plan, build and maintain the structure of a website.</p>
+                <a href="./contact-us/" className="btn primary"><FontAwesomeIcon icon={faDownload} /> Download CV</a>
+            </div>
         </div>
-        <div className="Clientele">
-          <div>client says</div>
-          <div>client says</div>
-        </div>
-      </div>
     </>
   )
 }
