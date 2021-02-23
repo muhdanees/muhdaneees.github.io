60a58baf961d092aca3722ddf027f6f7f20d83a6
m58045 <mohd.anees@htdigital.in>
2021-02-23 12:07:31 +0530

    little change

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownload, faToolbox } from "@fortawesome/free-solid-svg-icons";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
 function Home() {
   return (
     <>
