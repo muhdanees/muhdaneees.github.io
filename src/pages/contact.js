c4980333d5cec4dddaa2b6d9a9706cdb11f2a24d
m58045 <mohd.anees@htdigital.in>
2021-03-02 00:04:15 +0530

    changes for contact us

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMapMarker, faLocationArrow } from "@fortawesome/free-solid-svg-icons";
+import styles from "./contact.module.scss";
 function Contact() {
   return (
     <div className="content">
@@ -11,29 +12,31 @@ function Contact() {
                 </div>
             </div>
             <div className="sec-divs">
-                <div className="address-info"><FontAwesomeIcon icon={faMapMarker} /> <strong>Address</strong></div>
-                <p>New Delhi, India</p>
-                <hr />
-                <h2>Contact Form</h2>
-                <div className="formelement">
-                  <ul>
-                    <li><input type="text" placeholder="Enter Name" /></li>
-                    <li><input type="number" placeholder="Enter Number" /></li>
-                  </ul>
+              <div className="address-info"><FontAwesomeIcon icon={faMapMarker} /> <strong>Address</strong></div>
+              <p>New Delhi, India</p>
+              <hr />
+              <h2>Contact Us</h2>
+              <form className={styles.Form}>
+                <div className={`${styles.FormCol} ${styles.FormLabel}`}>
+                  <label className={styles.FormLabelCol}>
+                    <input className={styles.FormControl} type="text" placeholder="Enter Name" />
+                  </label>
+                  <label className={styles.FormLabelCol}>
+                    <input className={styles.FormControl} type="number" placeholder="Enter Number" />
+                  </label>
                 </div>
-                <div className="formelement">
-                  <ul>
-                    <li><input type="email" placeholder="Enter Email" /></li>
-                  </ul>
+                <div className={styles.FormCol}>
+                  <label>
+                    <input className={styles.FormControl} type="email" placeholder="Enter Email" />
+                  </label>
                 </div>
-                <div className="formelement">
-                  <ul>
-                    <li><textarea>Enter Message</textarea></li>
-                  </ul>
+                <div className={styles.FormCol}>
+                  <textarea className={styles.FormControl}>Enter Message</textarea>
                 </div>
-                <div className="formelement">
+                <div className={`${styles.FormCol} ${styles.SubContainer}`}>
                   <button type="button" className="btn primary"><FontAwesomeIcon icon={faLocationArrow} /> Send</button>
                 </div>
+              </form>
             </div>
         </div>
     </div>
