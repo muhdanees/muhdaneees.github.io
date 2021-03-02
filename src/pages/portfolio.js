f541010c46fa6dc00fdb2d4a0c2bab107955dfd6
m58045 <mohd.anees@htdigital.in>
2021-03-02 21:35:51 +0530

    portfolio

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -43,29 +43,151 @@ function Portfolio() {
                       </div>
                     </div>
                     <div className={styles.Item}>
-                      <img src="/hindustantimes.jpg" alt="" />
-                      <Link to="https://www.hindustantimes.com/">www.hindustantimes.com</Link>
+                      <img src="/hindustan.jpg" alt="" />
+                      <Link to="https://www.livehindustan.com/">www.livehindustan.com</Link>
                       <div className={styles.hoverItem}>
                         <FontAwesomeIcon icon={faRetweet} />
                         <button type="button">More Details...</button>
                       </div>
                     </div>
                     <div className={styles.Item}>
-                      <img src="/hindustantimes.jpg" alt="" />
-                      <Link to="https://www.hindustantimes.com/">www.hindustantimes.com</Link>
+                      <img src="/desimartini.jpg" alt="" />
+                      <Link to="https://www.desimartini.com/">www.desimartini.com</Link>
                       <div className={styles.hoverItem}>
                         <FontAwesomeIcon icon={faRetweet} />
                         <button type="button">More Details...</button>
                       </div>
                     </div>
                     <div className={styles.Item}>
-                      <img src="/hindustantimes.jpg" alt="" />
-                      <Link to="https://www.hindustantimes.com/">www.hindustantimes.com</Link>
+                      <img src="/dfurnishing.jpg" alt="" />
+                      <Link to="http://www.dfurnishingdreamz.com/">www.dfurnishingdreamz.com</Link>
                       <div className={styles.hoverItem}>
                         <FontAwesomeIcon icon={faRetweet} />
                         <button type="button">More Details...</button>
                       </div>
                     </div>
+                    <div className={styles.Item}>
+                      <img src="/fincalci.jpg" alt="" />
+                      <Link to="https://www.fincalci.com/">www.fincalci.com</Link>
+                      <div className={styles.hoverItem}>
+                        <FontAwesomeIcon icon={faRetweet} />
+                        <button type="button">More Details...</button>
+                      </div>
+                    </div>
+                    <div className={styles.Item}>
+                      <img src="/4square.jpg" alt="" />
+                      <Link to="http://4squarepromotions.com/">www.4squarepromotions.com</Link>
+                      <div className={styles.hoverItem}>
+                        <FontAwesomeIcon icon={faRetweet} />
+                        <button type="button">More Details...</button>
+                      </div>
+                    </div>
+                    <div className={styles.Item}>
+                      <img src="/dmagnus.jpg" alt="" />
+                      <Link to="http://www.dmagnuslighting.com/">www.dmagnuslighting.com</Link>
+                      <div className={styles.hoverItem}>
+                        <FontAwesomeIcon icon={faRetweet} />
+                        <button type="button">More Details...</button>
+                      </div>
+                    </div>
+                    <div className={styles.Item}>
+                      <img src="/zureka.jpg" alt="" />
+                      <Link to="http://www.Zureka.in/">www.Zureka.in</Link>
+                      <div className={styles.hoverItem}>
+                        <FontAwesomeIcon icon={faRetweet} />
+                        <button type="button">More Details...</button>
+                      </div>
+                    </div>
+                    <div className={styles.Item}>
+                      <img src="/healthscion.jpg" alt="" />
+                      <Link to="https://crm.healthscion.com/">crm.healthscion.com</Link>
+                      <div className={styles.hoverItem}>
+                        <FontAwesomeIcon icon={faRetweet} />
+                        <button type="button">More Details...</button>
+                      </div>
+                    </div>
+                    <div className={styles.Item}>
+                      <img src="/ipgprojects.jpg" alt="" />
+                      <Link to="https://admin.ipgprojects.com/">admin.ipgprojects.com</Link>
+                      <div className={styles.hoverItem}>
+                        <FontAwesomeIcon icon={faRetweet} />
+                        <button type="button">More Details...</button>
+                      </div>
+                    </div>
+                    <div className={styles.Item}>
+                      <img src="/energylineindia.jpg" alt="" />
+                      <Link to="https://www.energylineindia.com/">www.energylineindia.com</Link>
+                      <div className={styles.hoverItem}>
+                        <FontAwesomeIcon icon={faRetweet} />
+                        <button type="button">More Details...</button>
+                      </div>
+                    </div>
+                    <div className={styles.Item}>
+                      <img src="/indianpetrochem.jpg" alt="" />
+                      <Link to="https://indianpetrochem.com/">www.indianpetrochem.com</Link>
+                      <div className={styles.hoverItem}>
+                        <FontAwesomeIcon icon={faRetweet} />
+                        <button type="button">More Details...</button>
+                      </div>
+                    </div>
+                    <div className={styles.Item}>
+                      <img src="/excelaccessconsultant.jpg" alt="" />
+                      <Link to="https://excelaccessconsultant.com/">www.excelaccessconsultant.com</Link>
+                      <div className={styles.hoverItem}>
+                        <FontAwesomeIcon icon={faRetweet} />
+                        <button type="button">More Details...</button>
+                      </div>
+                    </div>
+
+                    <div className={styles.Item}>
+                      <img src="/access-programmer.jpg" alt="" />
+                      <Link to="http://www.access-programmer.org/">www.access-programmer.org</Link>
+                      <div className={styles.hoverItem}>
+                        <FontAwesomeIcon icon={faRetweet} />
+                        <button type="button">More Details...</button>
+                      </div>
+                    </div>
+
+                    <div className={styles.Item}>
+                      <img src="/sesi.jpg" alt="" />
+                      <Link to="https://www.sesi.in/">www.sesi.in</Link>
+                      <div className={styles.hoverItem}>
+                        <FontAwesomeIcon icon={faRetweet} />
+                        <button type="button">More Details...</button>
+                      </div>
+                    </div>
+                    
+
+                    <div className={styles.Item}>
+                      <img src="/makbog.jpg" alt="" />
+                      <Link to="http://makbog.com/">www.makbog.com</Link>
+                      <div className={styles.hoverItem}>
+                        <FontAwesomeIcon icon={faRetweet} />
+                        <button type="button">More Details...</button>
+                      </div>
+                    </div>
+
+
+                    <div className={styles.Item}>
+                      <img src="/samay.jpg" alt="" />
+                      <Link to="http://www.samaywatch.com/">www.samaywatch.com</Link>
+                      <div className={styles.hoverItem}>
+                        <FontAwesomeIcon icon={faRetweet} />
+                        <button type="button">More Details...</button>
+                      </div>
+                    </div>
+                    
+
+                    <div className={styles.Item}>
+                      <img src="/linkorg.jpg" alt="" />
+                      <Link to="http://www.link.org.in/">www.link.org.in</Link>
+                      <div className={styles.hoverItem}>
+                        <FontAwesomeIcon icon={faRetweet} />
+                        <button type="button">More Details...</button>
+                      </div>
+                    </div>
+
+
                   </div>
                 </div>
             </div>
