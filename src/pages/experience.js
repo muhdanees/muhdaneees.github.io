86a32ae5cb897fc8cd1b3198c27abdf7a650dc67
m58045 <mohd.anees@htdigital.in>
2021-03-05 18:00:01 +0530

    change for experience

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -33,25 +33,25 @@ function Experience() {
                 </li>
                 <li>
                   <strong>Indian Petro Group - </strong> <span>(A division of 360 Analytics &amp; Advisory Pvt. Ltd)</span>
-                  <p><strong>Position: </strong> Web Designer</p>
+                  <p><strong>Position: </strong> UI Developer</p>
                   <p><a href="http://www.indiapetro.com/" target="_blank" rel="noreferrer">www.indiapetro.com</a></p>
                   <p>6/2013 - 4/2015</p>
                 </li>
                 <li>
                   <strong>Oscillate Infotech Pvt. Ltd.</strong>
-                  <p><strong>Position: </strong> HTML Developer</p>
+                  <p><strong>Position: </strong> UI/UX Developer</p>
                   <p><a href="https://oscillateinfo.com/" target="_blank" rel="noreferrer">www.oscillateinfo.com</a></p>
                   <p>10/2012 - 6/2013</p>
                 </li>
                 <li>
                   <strong>Makbog Solutions Pvt. Ltd.</strong>
-                  <p><strong>Position: </strong> Web Designer</p>
+                  <p><strong>Position: </strong> UI Developer</p>
                   <p><a href="http://makbog.com/" target="_blank" rel="noreferrer">www.makbog.com</a></p>
                   <p>5/2011 - 10/2012</p>
                 </li>
                 <li>
                   <strong>Modulus Systems</strong>
-                  <p><strong>Position: </strong> Web Designer</p>
+                  <p><strong>Position: </strong> UI Designer/Developer</p>
                   <p>3/2009 - 4/2011</p>
                 </li>
               </ul>
